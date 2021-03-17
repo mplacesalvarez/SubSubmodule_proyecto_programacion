5a5e154b44056d6ef8d4ba4a8c1a7682b5d81b46
mplacesalvarez <mplacesalvarez@danielcastelaor.org>
2021-03-17 16:07:53 +0100

    commit 4

--- src/metodos/metodos_print.java
+++ src/metodos/metodos_print.java
@@ -1,7 +1,7 @@
 package metodos;
 
 public class metodos_print {
-    public void mensaje(){System.out.println("VACUNACIÓN COVID-19 EN ESPAÑA:\n1) Añadir persona.\n2) Añadir vacuna.\n3) Listado de personas.\n4) Buscar persona.\n5) Información vacunas.\n6) Realizar cambios.\n7) Salir.");}
+    public String mensaje(){return "VACUNACIÓN COVID-19 EN ESPAÑA:\n1) Añadir persona.\n2) Añadir vacuna.\n3) Listado de personas.\n4) Buscar persona.\n5) Información vacunas.\n6) Realizar cambios.\n7) Salir.";}
     public void mensaje1(){System.out.println("**** CAMBIOS REALIZADOS ****");}
     public void mensaje3(){System.out.println("**** RESULTADO DE LA BUSQUEDA ****");}
     public void mensaje2(){System.out.println("No hay personas en la lista.");}
