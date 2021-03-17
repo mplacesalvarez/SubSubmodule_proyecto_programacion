80c9fe612c77ec8b3d233093894faccdd1054d7f
mplacesalvarez <mplacesalvarez@danielcastelaor.org>
2021-03-17 15:39:42 +0100

    Initial commit

---
+++ src/Metodos.java
@@ -0,0 +1,10 @@
+public class Metodos {
+    public void mensaje(){System.out.println("VACUNACIÓN COVID-19 EN ESPAÑA:\n1) Añadir persona.\n2) Añadir vacuna.\n3) Listado de personas.\n4) Buscar persona.\n5) Información vacunas.\n6) Realizar cambios.\n7) Salir.");}
+    public void mensaje1(){System.out.println("**** CAMBIOS REALIZADOS ****");}
+    public void mensaje3(){System.out.println("**** RESULTADO DE LA BUSQUEDA ****");}
+    public void mensaje2(){System.out.println("No hay personas en la lista.");}
+    public void mensaje4(){System.out.println("No hay personas en la lista con ese nombre.");}
+    public void mensaje5(){System.out.println("No hay personas en la lista con ese apellido.");}
+    public void mensaje6(){System.out.println("No hay personas en la lista con ese DNI.");}
+
+}
